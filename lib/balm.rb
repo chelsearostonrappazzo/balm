3f7ac902f776b4d19ee4cb366ceb0d5be1e996d0
Chelsea Roston <cmroston@gmail.com>
2021-03-06 15:25:14 -0500

    creates gem files and updates gemspec

---
+++ lib/balm.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "balm/version"
+
+module Balm
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/balm/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Balm
+  VERSION = "0.1.0"
+end
